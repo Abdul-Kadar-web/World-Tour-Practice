0c63a411c191c59e7e9aeb7adec8f0d8a101472c
agforhad <abdulgaffer1100@gmail.com>
2021-02-06 15:46:41 +0600

    World-Tour_practice

---
+++ app.js
@@ -0,0 +1,74 @@
+fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data));
+
+
+const displayCountries = countries => {
+    const countriesDiv = document.getElementById('countries')
+
+    // for each
+    countries.forEach(country => {
+        const countryDiv = document.createElement('div');
+        countryDiv.className = 'country';
+        const countryInfo = `
+            <h3 class ="country-name">${country.name}</h3>
+            <p>${country.capital}</p>
+            <button onclick="displayCountryDetail('${country.name}')" >Details</button>
+        `;
+        countryDiv.innerHTML = countryInfo;
+        countriesDiv.appendChild(countryDiv);
+    });
+}
+
+const displayCountryDetail = name =>{
+    const url = `
+    https://restcountries.eu/rest/v2/name/${name}`
+    fetch(url)
+    .then(res => res.json())
+    // .then(data => console.log(data[0].name));
+    .then(data => renderCountryInfo(data[0]));
+
+}
+
+const renderCountryInfo = country => {
+    const countryDiv =document.getElementById('countryDetail');
+    countryDiv.innerHTML = `
+        <h1>${country.name}</h1>
+        <p>Population: ${country.population}</p>
+        <p>Area: ${country.area}</p>
+        <img src="${country.flag}"></img>
+    `
+}
+
+    // const displayCountries = countries => {
+    //     const countriesDiv = document.getElementById('countries')
+
+    // for loop
+    // for (let i = 0; i < countries.length; i++) {
+    //     const country = countries[i];
+    //     const countryDiv = document.createElement('div');
+
+
+    //     const h3 = document.createElement('h3');
+    //     h3.innerText = country.name;
+    //     const p = document.createElement('p');
+    //     p.innerText = country.capital;
+    //     countryDiv.appendChild(h3);
+    //     countryDiv.appendChild(p);
+    //     countriesDiv.appendChild(countryDiv);
+
+    //     uporer tar short version
+
+    //     countryDiv.className = 'country'
+    //     const countryInfo = `
+    //         <h3 class ="country-name">${country.name}</h3>
+    //         <p>${country.capital}</p>
+    //     `;
+    
+    //     countryDiv.innerHTML = countryInfo;
+    //     countriesDiv.appendChild(countryDiv);
+
+    //     console.log(country.name);
+    // }
+
+// }
